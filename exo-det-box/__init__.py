2832feefdbf47c8d650f73935a549eff4337dfc5
Dean Keithly <drk94@cornell.edu>
2021-03-25 22:41:53 -0400

    adding init file

---
+++ exo-det-box/__init__.py
@@ -0,0 +1,6 @@
+name = "exo-det-box"
+__version__ = '0.0.1'
+# Set up a default logging handler to avoid "No handler found" warnings.
+# Other handlers can add to this one.
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
